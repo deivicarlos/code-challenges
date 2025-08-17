9b7f3f84d661733dde1184ba2c7ac7d928ffdde8
Carlos Garcia <carlos.garcia@veerum.com>
2025-08-17 14:41:29 -0600

    feat: added chakra and tankstack

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from './components/ui/provider.tsx';
 import App from './App.tsx';
+
+const qc = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={qc}>
+      <Provider>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 );
